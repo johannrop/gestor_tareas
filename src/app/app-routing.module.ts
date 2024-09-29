2b9a2e5d0861f3dc244432b00a8dfa99cd55c3d7
johannrop <johann.ortiz@unipamplona.edu.co>
2024-09-29 13:12:22 -0500

    generado componente start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './components/start/start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'app-start',
+    pathMatch: 'full',
+  },
+  {
+    path: 'app-start',
+    component: StartComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
