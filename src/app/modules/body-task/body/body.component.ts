2e72ae87fbd0cabfe5bd21aa4c48f31ec5be7e0a
johannrop <johann.ortiz@unipamplona.edu.co>
2024-09-30 15:21:42 -0500

    generada logica para guardar una tarea

--- src/app/modules/body-task/body/body.component.ts
+++ src/app/modules/body-task/body/body.component.ts
@@ -1,42 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.scss']
 })
-export class BodyComponent implements OnInit{
-  habilidades: string[] = ['']; 
-  personas: string[] = ['']; 
-  habilidadesForm!: FormGroup;
+export class BodyComponent implements  OnInit {
+  tareaForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.habilidadesForm = new FormGroup({
-      nombreHabilidad: new FormControl('', [Validators.required, Validators.minLength(4)])  
+    this.tareaForm = this.fb.group({
+      name: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      personas: this.fb.array([this.crearPersona()])
     });
   }
 
+  get personas(): FormArray {
+    return this.tareaForm.get('personas') as FormArray;
+  }
+
+  getHabilidades(indexPersona: number): FormArray {
+    return this.personas.at(indexPersona).get('habilidades') as FormArray;
+  }
+
+  crearPersona(): FormGroup {
+    return this.fb.group({
+      nameC: ['', [Validators.required]],
+      age: ['', [Validators.required]],
+      habilidades: this.fb.array([this.fb.control('', Validators.required)])
+    });
+  }
+
+  agregarPersona() {
+    this.personas.push(this.crearPersona());
+  }
+
+  eliminarPersona(index: number) {
+    if (this.personas.length > 1) {
+      this.personas.removeAt(index);
+    }
+  }
+
+  agregarHabilidad(indexPersona: number) {
+    const habilidades = this.getHabilidades(indexPersona);
+    habilidades.push(this.fb.control('', Validators.required));
+  }
+
+  eliminarHabilidad(indexPersona: number, indexHabilidad: number) {
+    const habilidades = this.getHabilidades(indexPersona);
+    if (habilidades.length > 1) {
+      habilidades.removeAt(indexHabilidad);
+    }
+  }
+
   onSubmit() {
-    console.log(this.habilidadesForm.value);  
-    console.log("habilidades= " + this.habilidades); 
-    }
-
-  agregarHabilidad(index: number) {
-     console.log("habilidades= " + this.habilidades); 
-    if (this.habilidades[index]) {
-      this.habilidades.push('');  
-
-    }
-  }
-
-  eliminarHabilidad(index: number) {
-    if (this.habilidades.length > 1) {
-      this.habilidades.splice(index, 1);  
-    }
-  }
-
-  trackByIndex(index: number, item: any): number {
-    return index;
+    console.log(this.tareaForm.value);
   }
 }
